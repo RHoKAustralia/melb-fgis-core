2b4a50ffce29806e1b26e29481bcb1fea79875c5
Mark Wolfe <mark@wolfe.id.au>
2013-02-09 14:24:11 +1100

    Added socket.io.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express')
     , configurations = module.exports
     , app = express()
+    , http = require('http')
+    , server = http.createServer(app)
+    , io = require('socket.io').listen(server)
     , nconf = require('nconf')
     , winston = require('winston')
 
@@ -16,6 +19,9 @@ nconf.argv().env().file({ file: 'local.json' })
 // Routes
 require('./routes')(app)
 
+// Socket IO
+require('./lib/socketio')(io)
+
 logger.info('listening on', nconf.get('port'))
 
-app.listen(process.env.PORT || nconf.get('port'))
+server.listen(process.env.PORT || nconf.get('port'))

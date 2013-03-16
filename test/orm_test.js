a22bbe9977eb337b310db62a638592235e5694c5
Andras Bubics <abubics@dius.com.au>
2013-03-16 15:22:53 +1100

    - Fix for Express not throwing connection error. - Add Postgres module.

--- test/orm_test.js
+++ test/orm_test.js
@@ -1,23 +1,29 @@
 var should = require('should')
 , fgisOrm = require('../lib/fgis-orm')
+, express = require('express');
 
+describe('orm', function() {
 
-describe('orm', function(){
+  var app = express();
+  var conf = {
+    get: function(key) {
+      console.log("get", key);
+      var password = process.env.DEV_POSTGRES_PASSWORD;
+      if (typeof password == 'undefined') {
+        throw Error('Missing env.DEV_POSTGRES_PASSWORD');
+      }
+      var dbName = 'fgis_development';
+      var dbOwner = 'fgis_development';
+      return "postgres://" + dbOwner + ":" + password + "@localhost/" + dbName;
+    }
+  };
 
-  it('should load the orm', function(done){
+  it('should load the orm', function(done) {
+    console.log('before');
+    fgisOrm(app, conf);
+    console.log('after');
+    //TODO: assert that the ORM is actually loaded
+    done()
+  });
 
-    //var fgisOrm = new FgisOrm(/* inject stuff later */);
-
-    fgisOrm.getEvents('beer', function(err, result){
-
-      // assert / read results
-
-
-
-      done();
-
-    })
-
-  })
-
-})
+});

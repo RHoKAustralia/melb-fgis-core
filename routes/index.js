4e2163d6a9c9b5432aa1988bc39a0ba05d79c506
Andras Bubics <abubics@dius.com.au>
2013-06-01 15:16:15 +1000

    - Move orm methods into index (not fgis-orm).

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,57 @@
 module.exports = function(app) {
 
-    // Home/main
-    app.get('/', function(req, res) {
+  var localApp = app;
 
-      req.db.models.feature.find({"description": ""},  function(err, features){
+  function logObject(obj) {
+    console.log('obj', require('util').inspect(obj));
+  }
 
-      })
+  function addFeature(feature, db, cb) {
+    logObject(feature);
 
-        res.render('index', { title: 'fgis-core' })
-    })
+    db.models.feature.create([
+      {
+        'description': feature.description
+      , 'geo':         feature.geo
+      , 'updatedAt':   new Date().getTime()
+      }
+    ], function(err, items) {
+      console.log(items);
+      if (err) {
+        cb(err);
+        return;
+      }
+      var successIds = [];
+      for (var i = 0; i < items.length; i++) {
+        console.log(items[i]);
+        successIds.push(items[i].id);
+      };
+      cb(null, successIds);
+    });
+  }
+
+  app.get('/', function(req, res) {
+    req.db.models.feature.find({'description': ''},  function(err, features) {})
+    res.render('index', { title: 'fgis-core' })
+  })
+
+  app.get('/feature/fire', function(req, res) {
+    console.log('GET ' + req.url);
+
+    var features = req.db.models.feature.find({'type':'fire'});
+    res.send(features);
+  });
+
+  app.post('/feature/fire', function(req, res) {
+    console.log('POST ' + req.url);
+
+    addFeature(req.body, req.db, function(error, featureIds) {
+      if (error) {
+        res.send(500, error);
+      } else {
+        res.send({'featuresAdded': featureIds});
+      }
+    });
+  });
 
 }

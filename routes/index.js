d41de8113a039ddaeff3c3008acd9c450c0284f1
Andras Bubics <abubics@dius.com.au>
2013-06-22 21:21:48 +1000

    - Turn explicit REST methods into implicit convention methods with optional overrides. - Remove some repetetive copypasta to match REST convention.

--- routes/index.js
+++ routes/index.js
@@ -3,49 +3,8 @@ var feature = require('../lib/features.js');
 
 module.exports = function(app) {
 
-  /**
-   * Check error and build generic http response for a single object.
-   *
-   * @param res
-   * @param err
-   * @param data
-   */
-  function singleResponse(res, err, data) {
-    if (err) {
-      res.send(500, err);
-    } else {
-      res.send(data);
-    }
-  }
-
   app.get('/', function(req, res) {
     res.render('index', { title: 'fgis-core' });
-  });
-
-  app.get('/feature/fire', function(req, res) {
-    feature.allFires(req.db, singleResponse.bind(this, res));
-  });
-  app.get('/feature/fire/:id', function(req, res) {
-    feature.getFire(req.db, req.params.id, singleResponse.bind(this, res));
-  });
-  app.post('/feature/fire', function(req, res) {
-    feature.addFire(req.body, req.db, function(err, featureId) {
-      if (err) {
-        res.send(500, err);
-      } else {
-        res.send(201, { id: featureId });
-      }
-    });
-  });
-  app.delete('/feature/fire/:id', function(req, res) {
-    log(req.method, req.url);
-    feature.deleteFire(req.db, req.params.id, function(err, fire) {
-      if (err) {
-        res.send(500, err);
-      } else {
-        res.send();
-      }
-    });
   });
 
   var defaultResponse = function(res) {
@@ -58,11 +17,11 @@ module.exports = function(app) {
     }
   }
   var defaultAddResponse = function(res) {
-    return function(err, featureId) {
+    return function(err, featureInfo) {
       if (err) {
         res.send(500, err);
       } else {
-        res.send(201, { id: featureId });
+        res.send(201, featureInfo);
       }
     }
   }
@@ -76,23 +35,27 @@ module.exports = function(app) {
     }
   }
 
-  app.get('/feature/location', function(req, res) {
-    feature.allLocations(req.db, defaultResponse(res));
-  });
-  app.get('/feature/location/:id', function(req, res) {
-    feature.getLocation(req.db, req.params.id, defaultResponse(res));
-  });
-  app.post('/feature/location', function(req, res) {
-    feature.addLocation(req.body, req.db, defaultAddResponse(res));
-  });
-  app.delete('/feature/location/:id', function(req, res) {
-    feature.deleteLocation(req.db, req.params.id, defaultDeleteResponse(res));
-  });
-
   app.get('/feature', function(req, res) {
     feature.all(req.db, defaultResponse(res));
   });
 
-  //TODO: '/feature/:type/:id' etc
+  app.get('/feature/:featureType', function(req, res) {
+    var ft = req.params.featureType;
+    feature[ft].getAll(req.db, defaultResponse(res));
+  });
+  app.get('/feature/:featureType/:id', function(req, res) {
+    var ft = req.params.featureType;
+    var id = req.params.id;
+    feature[ft].getOne(req.db, id, defaultResponse(res));
+  });
+  app.post('/feature/:featureType', function(req, res) {
+    var ft = req.params.featureType;
+    feature[ft].addOne(req.db, req.body, defaultAddResponse(res));
+  });
+  app.delete('/feature/:featureType/:id', function(req, res) {
+    var ft = req.params.featureType;
+    var id = req.params.id;
+    feature[ft].delOne(req.db, id, defaultDeleteResponse(res));
+  });
 
 };

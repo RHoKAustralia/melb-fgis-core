60fa043f40b13dfe42d476332f21b3f06058a188
Karl Glaser <kg@papercloud.com.au>
2013-12-07 15:36:39 +1100

    Features are rendered on leaflet

--- public/js/site.js
+++ public/js/site.js
@@ -1,10 +1,10 @@
 (function($) {
 
-  function toggleDataFrame(dataFrameId, title){
+  function toggleDataFrame(dataFrameId, title) {
 
     var idSelector = '#' + dataFrameId;
 
-    if($(idSelector).hasClass('selected')) {
+    if ($(idSelector).hasClass('selected')) {
       $('#bottom-menu').find('a.selected').removeClass('selected');
       $('#data-frame').fadeOut('fast');
     } else {
@@ -17,97 +17,94 @@
 
   }
 
-  var models =
-    {
-      Poi: Backbone.Model.extend(
-        {
-          defaults:
-            {
-              id: undefined
-            , lat: undefined
-            , long: undefined
-            }
-        }
-      )
-    }
+  var models = {
+    Poi: Backbone.Model.extend({})
+  }
 
-  var collections =
-    {
-      Pois: Backbone.Collection.extend(
-        {
-          model: models.Poi
-        }
-      )
-    }
+  var collections = {
+    Pois: Backbone.Collection.extend({
+      model: models.Poi,
+      url: '/feature',
+    })
+  }
 
-  var views =
-    {
-      Home: Backbone.View.extend(
-        {
-          initialize: function(options) {
-            _.bindAll(this)
+  var views = {
+    Home: Backbone.View.extend({
+      initialize: function(options) {
+        _.bindAll(this)
 
-            this.pois = options.pois
+        this.pois = options.pois
 
-            this.pois.on('add', this.addMarkers)
-            this.pois.on('change', this.changeMarker)
+        this.pois.on('add', this.addMarkers)
+        this.pois.on('change', this.changeMarker)
 
-            this.markers = {}
+        this.markers = {}
+      },
+      styleMap: {
+        'fire': {
+          style: {
+            color: "#EE0000",
+            weight: 5,
+            opacity: 0.65
           }
-        , render: function() {
-            $('#critical').on('click', this.critical);
-            $('#overlay').on('click', this.overlay);
-            $('#team').on('click', this.team);
-            $('#weather').on('click', this.weather);
-            $('#feed').on('click', this.feed);
-            this.map = L.map('map').setView([-37.793566209439, 144.94111608134], 14)
-
-            L.tileLayer('http://{s}.tile.cloudmade.com/aeb94991e883413e8262bd55def34111/997/256/{z}/{x}/{y}.png',
-              {
-                attribution: 'Made with love at <a href="https://github.com/vertis/rhok-fgis/">RHoK Melbourne</a>, Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
-              , maxZoom: 18
-              }
-            ).addTo(this.map)
-            return this
-          }
-        , addMarkers: function() {
-            _.each(this.pois.models, this.addMarker)
-          }
-        , addMarker: function(poi) {
-            this.markers[poi.id] = this.markers[poi.id] || L.marker([poi.attributes.lat, poi.attributes.long]).addTo(this.map)
-          }
-        , changeMarker: function(poi) {
-            this.markers[poi.id].setLatLng([poi.attributes.lat, poi.attributes.long])
-          }
-        , critical: function() {
-            $('#top-menu').find('a.selected').removeClass('selected');
-            $('#critical').addClass('selected');
-          }
-        , overlay: function() {
-            $('#top-menu').find('a.selected').removeClass('selected');
-            $('#overlay').addClass('selected');
-          }
-        , team: function() {
-            toggleDataFrame('team', 'Team Test')
-          }
-        , weather: function() {
-            toggleDataFrame('weather', 'Weather Test')
-          }
-        , feed: function() {
-            toggleDataFrame('feed', 'Feed Test')
+        },
+        'location': {
+          pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng, {
+              color: "#0000FF",
+              fillOpacity: 0.8
+            });
           }
         }
-      )
-    }
+      },
+      render: function() {
+        $('#critical').on('click', this.critical);
+        $('#overlay').on('click', this.overlay);
+        $('#team').on('click', this.team);
+        $('#weather').on('click', this.weather);
+        $('#feed').on('click', this.feed);
+        this.map = L.map('map').setView([-37.793566209439, 144.94111608134], 14)
 
-  var Router = Backbone.Router.extend(
-    {
-      routes:
-        {
-          '': 'home'
-        }
+        L.tileLayer('http://{s}.tile.cloudmade.com/aeb94991e883413e8262bd55def34111/997/256/{z}/{x}/{y}.png', {
+          attribution: 'Made with love at <a href="https://github.com/vertis/rhok-fgis/">RHoK Melbourne</a>, Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+          maxZoom: 18
+        }).addTo(this.map)
+        return this
+      },
+      addMarkers: function() {
+        _.each(this.pois.models, this.addMarker)
+      },
+      addMarker: function(poi) {
+        L.geoJson(poi.get('geo'), this.styleMap[poi.get('type')]).addTo(this.map);
+      },
+      changeMarker: function(poi) {
+        // this.markers[poi.id].setLatLng([poi.attributes.lat, poi.attributes.long])
+      },
+      critical: function() {
+        $('#top-menu').find('a.selected').removeClass('selected');
+        $('#critical').addClass('selected');
+      },
+      overlay: function() {
+        $('#top-menu').find('a.selected').removeClass('selected');
+        $('#overlay').addClass('selected');
+      },
+      team: function() {
+        toggleDataFrame('team', 'Team Test')
+      },
+      weather: function() {
+        toggleDataFrame('weather', 'Weather Test')
+      },
+      feed: function() {
+        toggleDataFrame('feed', 'Feed Test')
+      }
+    })
+  }
+
+  var Router = Backbone.Router.extend({
+    routes: {
+      '': 'home'
     }
-  )
+  })
 
   $(function() {
 
@@ -132,18 +129,18 @@
     // Let's test it...
 
     // subscribe to stuff
-//    app.socket = io.connect('http://localhost:3000')
+    //    app.socket = io.connect('http://localhost:3000')
     console.log('loading', 'primus')
     window.primus = Primus.connect('ws://localhost:3000');
 
-/*
+    /*
     app.socket.on('watchEvents', function(data){
 
       console.log(data)
 
       app.event = data.event;
 
-      L.geoJson(event.featureCollection).addTo(app.views.map)
+    L.geoJson(event.featureCollection).addTo(app.views.map)
 
       // update the map
 
@@ -159,19 +156,17 @@
 
 
     setTimeout(function() {
+      app.pois.fetch({
+        update: true
+      });
 
-      app.pois.add(
-        {
-          id: 1234
-        , lat: -37.793566209439
-        , long: 144.94111608134
-        }
-      )
-    }, 3000)
+    }, 300)
 
-    setTimeout(function() {
-      app.pois.get(1234).set({lat: -37.803566209439})
-    }, 4000)
+    // setTimeout(function() {
+    // app.pois.get(1234).set({
+    //   lat: -37.803566209439
+    // })
+    // }, 4000)
 
   })
 
